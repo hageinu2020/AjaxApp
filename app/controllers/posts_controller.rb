c54d5c77b5f10155441a5ac6bbcae4023212560b
hageinu2020 <clockwork.orange87@icloud.com>
2020-08-03 14:53:34 +0900

    既読機能に必要な処理をルーティングに記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,26 @@
 class PostsController < ApplicationController
+  
   def index  # indexアクションを定義した
-    @posts = Post.all  #全てのレコードを@postsに代入
+    @posts = Post.all.order(id: "DESC")  #全てのレコードを@postsに代入
   end
 
-  def new
-  end
+  #def new
+  #end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
+
+  def checked
+    post = post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item}
 end
 
